46ddefbf26d736334a64411134027ea6113fe8f6
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-16 16:50:43 +0200

    Create event_attending modul and migrate it to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_131639) do
+ActiveRecord::Schema.define(version: 2021_06_16_144046) do
+
+  create_table "event_attendings", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["attended_event_id"], name: "index_event_attendings_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_event_attendings_on_attendee_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
