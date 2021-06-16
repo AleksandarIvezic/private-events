9f5ab4513cba7b8b0b3e00fdf727f155490a79c9
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-16 15:20:54 +0200

    Update events controller and migrate creator_id to events table

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,27 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
     def index
         @events=Event.all
+    end
+
+    def new
+        @event = current_user.events.build
+    end
+
+    def create
+        @event = current_user.events.build(event_params)
+
+        if @event.save
+            redirect_to "users#show"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:title,:details,:event_date)
     end
 
 end
