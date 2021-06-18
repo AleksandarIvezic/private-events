41329ff74ffc471e6e312facfe063f1399519516
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-18 15:26:52 +0200

    Create past and upcoming class methods on the Event model and use it on Event index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def show
         @events= current_user.events
+        @attended_events = current_user.attended_events
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     has_many :event_attendings, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendings
+
+    def self.past
+        where("event_date < ?", DateTime.now) 
+    end
+
+    def self.upcoming 
+        where("event_date > ?", DateTime.now)
+    end
 end
