14355edc2ef701b0a2560d3dbc78c307588dbdb3
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-18 23:12:23 +0200

    Add source to attendees and attended_events in User/Event models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :events, foreign_key: 'creator_id', dependent: :destroy
   has_many :event_attendings, foreign_key: :attendee_id, dependent: :destroy
-  has_many :attended_events, through: :event_attendings
+  has_many :attended_events, through: :event_attendings, source: :attended_event
 end
