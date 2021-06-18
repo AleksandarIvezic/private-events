d731571a782467d89efd267e41cf5b4580cf9bcd
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-18 22:34:00 +0200

    Use past/upcoming scopes inside Users controller and refactor Users show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @events = current_user.events
-    @attended_events = current_user.attended_events
+    @past_events = current_user.attended_events.past
+    @upcoming_events = current_user.attended_events.upcoming
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   validates :event_date, presence: true
   belongs_to :creator, class_name: 'User'
   has_many :event_attendings, foreign_key: :attended_event_id, dependent: :destroy
-  has_many :attendees, through: :event_attendings
+  has_many :attendees, through: :event_attendings, source: :attendee
   scope :past, -> { where('event_date < ?', DateTime.now) }
   scope :upcoming, -> { where('event_date > ?', DateTime.now) }
 end
